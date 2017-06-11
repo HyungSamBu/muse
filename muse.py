849c82aef6ae52e33d34d727a83467aaf35e8e6b
HyungSamBu <vossmk@gmail.com>
2017-06-11 18:51:44 -0400

    Trying an elif statement

--- muse.py
+++ muse.py
@@ -26,7 +26,7 @@ def handle_command(command, channel):
                "* command with numbers, delimited by spaces."
     if command.startswith(EXAMPLE_COMMAND):
         response = "Sure...write some more code then I can do that!"
-    else if command.startswith(TIMER_REQUEST):
+    elif command.startswith(TIMER_REQUEST):
     	set_time= int(re.search(r'\d+', command).group())
 		response= set_time
     slack_client.api_call("chat.postMessage", channel=channel,
